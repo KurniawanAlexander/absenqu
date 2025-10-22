8bfebdb7b5031c6006f2510fb0c7ad5dcad6a8fe
Kurniawan Alexander <kurniawanalexander26@gmail.com>
2025-10-22 12:20:05 +0700

    update logo

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,10 +1,12 @@
   <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start"
           style="background-color: #34FFF1;">
-          <a class="navbar-brand brand-logo me-5" href="index.html"><img src="{{ asset('assets/images/logo.svg') }}"
-                  class="me-2" alt="logo" /></a>
-          <a class="navbar-brand brand-logo-mini" href="index.html"><img src="{{ asset('assets/images/logo-mini.svg') }}"
-                  alt="logo" /></a>
+
+          <a class="navbar-brand brand-logo me-5">
+
+              <img src="{{ asset('assets/images/logo1.png') }}" style="height: 50px; width: 200px;" alt="logo" />
+
+          </a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end" style="background-color: #34FFF1;">
           <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
