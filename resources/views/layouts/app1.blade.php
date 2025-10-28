d1ccde5c2dd66ddb53719030a8088e5cc8ea1821
Kurniawan Alexander <kurniawanalexander26@gmail.com>
2025-10-28 16:37:17 +0700

    update tampilan template

--- resources/views/layouts/app1.blade.php
+++ resources/views/layouts/app1.blade.php
@@ -78,16 +78,18 @@
                     <div class="page-title">
                         <div class="row">
                             <div class="col-6">
-                                <h4>Default</h4>
+                                <h4>@yield('title')</h4>
                             </div>
                             <div class="col-6">
                                 <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="index.html">
+                                    <li class="breadcrumb-item"><a href="/admin">
                                             <svg class="stroke-icon">
-                                                <use href="../assets/svg/icon-sprite.svg#stroke-home"></use>
+                                                <use
+                                                    href="{{ asset('assets/riho-asset/svg/icon-sprite.svg#stroke-home') }}">
+                                                </use>
                                             </svg></a></li>
-                                    <li class="breadcrumb-item">Dashboard</li>
-                                    <li class="breadcrumb-item active">Default</li>
+                                    <!-- <li class="breadcrumb-item">Dashboard</li> -->
+                                    <li class="breadcrumb-item active">@yield('title')</li>
                                 </ol>
                             </div>
                         </div>
