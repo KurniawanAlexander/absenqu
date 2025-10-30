b9a5a493f48031a1bbc082b5752e379768107c69
Kurniawan Alexander <kurniawanalexander26@gmail.com>
2025-10-30 12:03:43 +0700

    update tampilan template

--- resources/views/dashboard1.blade.php
+++ resources/views/dashboard1.blade.php
@@ -113,6 +113,10 @@
         .table-list tr:nth-child(even) {
             background-color: #f8f9fa;
         }
+        .table-container {
+            max-height: 400px;
+            overflow-y: auto;
+        }
     </style>
 
     <div class="widget-container">
@@ -154,6 +158,7 @@
         <div class="main-content-row">
             <div class="main-card" style="flex: 2;">
                 <h4>Pengguna Baru Hari Ini</h4>
+                <div class="table-container">
                 <table class="table-list">
                     <thead>
                         <tr>
@@ -188,12 +193,164 @@
                             <td>eko.w@mail.com</td>
                             <td>5 jam lalu</td>
                         </tr>
+                        <tr>
+                            <td>Andi Wijaya</td>
+                            <td>andi.w@mail.com</td>
+                            <td>5 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Rina Sari</td>
+                            <td>rina.s@mail.com</td>
+                            <td>6 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Joko Susilo</td>
+                            <td>joko.s@mail.com</td>
+                            <td>6 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Lina Marlina</td>
+                            <td>lina.m@mail.com</td>
+                            <td>7 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Agus Setiawan</td>
+                            <td>agus.s@mail.com</td>
+                            <td>8 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Fitriani</td>
+                            <td>fitriani@mail.com</td>
+                            <td>8 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Heru Prasetyo</td>
+                            <td>heru.p@mail.com</td>
+                            <td>9 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Indah Permata</td>
+                            <td>indah.p@mail.com</td>
+                            <td>9 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Rudi Hartono</td>
+                            <td>rudi.h@mail.com</td>
+                            <td>10 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Sari Puspita</td>
+                            <td>sari.p@mail.com</td>
+                            <td>10 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Teguh Santoso</td>
+                            <td>teguh.s@mail.com</td>
+                            <td>11 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Wulan Dari</td>
+                            <td>wulan.d@mail.com</td>
+                            <td>11 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Yoga Pratama</td>
+                            <td>yoga.p@mail.com</td>
+                            <td>12 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Zainal Abidin</td>
+                            <td>zainal.a@mail.com</td>
+                            <td>12 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Putri Amelia</td>
+                            <td>putri.a@mail.com</td>
+                            <td>13 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Bambang Pamungkas</td>
+                            <td>bambang.p@mail.com</td>
+                            <td>14 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Citra Lestari</td>
+                            <td>citra.l@mail.com</td>
+                            <td>14 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Dian Nugraha</td>
+                            <td>dian.n@mail.com</td>
+                            <td>15 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Eka Putri</td>
+                            <td>eka.p@mail.com</td>
+                            <td>15 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Fajar Sidik</td>
+                            <td>fajar.s@mail.com</td>
+                            <td>16 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Gita Gutawa</td>
+                            <td>gita.g@mail.com</td>
+                            <td>16 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Hendra Gunawan</td>
+                            <td>hendra.g@mail.com</td>
+                            <td>17 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Ika Kartika</td>
+                            <td>ika.k@mail.com</td>
+                            <td>17 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Jefri Nichol</td>
+                            <td>jefri.n@mail.com</td>
+                            <td>18 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Kartika Sari</td>
+                            <td>kartika.s@mail.com</td>
+                            <td>18 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Lukman Hakim</td>
+                            <td>lukman.h@mail.com</td>
+                            <td>19 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Maya Indah</td>
+                            <td>maya.i@mail.com</td>
+                            <td>19 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Nanda Pratama</td>
+                            <td>nanda.p@mail.com</td>
+                            <td>20 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Oscar Lawalata</td>
+                            <td>oscar.l@mail.com</td>
+                            <td>20 jam lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Pandu Wijaya</td>
+                            <td>pandu.w@mail.com</td>
+                            <td>21 jam lalu</td>
+                        </tr>
                     </tbody>
                 </table>
+                </div>
             </div>
 
             <div class="main-card" style="flex: 1;">
                 <h4>Toko Baru Dibuat</h4>
+                <div class="table-container">
                 <table class="table-list">
                     <thead>
                         <tr>
@@ -218,8 +375,89 @@
                             <td>Aneka Gadget</td>
                             <td>1 hari lalu</td>
                         </tr>
+                        <tr>
+                            <td>Warung Serba Ada</td>
+                            <td>2 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Karya Mandiri Teknik</td>
+                            <td>2 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Sumber Rejeki</td>
+                            <td>3 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Toko Kelontong Barokah</td>
+                            <td>3 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Maju Jaya Abadi</td>
+                            <td>4 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Sinar Terang</td>
+                            <td>4 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Berkah Tani</td>
+                            <td>5 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Lancar Jaya Motor</td>
+                            <td>5 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Putra Bangsa Collection</td>
+                            <td>6 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Cahaya Abadi</td>
+                            <td>6 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Griya Busana Muslim</td>
+                            <td>7 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Istana Sepatu</td>
+                            <td>7 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Jagonya Komputer</td>
+                            <td>8 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Kencana Motor</td>
+                            <td>8 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Lestari Alam</td>
+                            <td>9 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Mega Elektronik</td>
+                            <td>9 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Nusantara Craft</td>
+                            <td>10 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Optik Sehat</td>
+                            <td>10 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Pustaka Ilmu</td>
+                            <td>11 hari lalu</td>
+                        </tr>
+                        <tr>
+                            <td>Ratu Dapur</td>
+                            <td>11 hari lalu</td>
+                        </tr>
                     </tbody>
                 </table>
+                </div>
             </div>
         </div>
     </div>
